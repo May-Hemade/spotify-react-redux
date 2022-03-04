a05d4b786e152cb0e474924a082de498d02684f4
May Hemade <71593691+May-Hemade@users.noreply.github.com>
2022-03-04 11:50:40 +0200

    develop 1

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -3,3 +3,21 @@ import { createStore, combineReducers, compose, applyMiddleware } from "redux"
 import thunk from "redux-thunk"
 
 const aComposeFunction = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+export const initialState = {
+  home: {
+    rockSongs: [],
+    popSongs: [],
+    hipHopSongs: [],
+  },
+}
+
+const bigReducer = combineReducers({
+  home: homeReducer,
+})
+
+export const configureStore = createStore(
+  bigReducer,
+  initialState,
+  aComposeFunction(applyMiddleware(thunk))
+)
