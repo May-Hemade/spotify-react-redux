9d1c8b50b98465abac4a75ebdb028b2dead99c40
May Hemade <71593691+May-Hemade@users.noreply.github.com>
2022-03-04 18:15:15 +0200

    fixed like

--- src/components/AlbumCard.jsx
+++ src/components/AlbumCard.jsx
@@ -1,24 +1,24 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { AiFillHeart } from "react-icons/ai";
-import { BsHeart } from "react-icons/bs";
-import { connect } from "react-redux";
-import { addToLikedAction } from "../redux/actions";
-import { removeFromLikedAction } from "../redux/actions";
+import React from "react"
+import { Link } from "react-router-dom"
+import { AiFillHeart } from "react-icons/ai"
+import { BsHeart } from "react-icons/bs"
+import { connect } from "react-redux"
+import { addToLikedAction } from "../redux/actions"
+import { removeFromLikedAction } from "../redux/actions"
 const mapStateToProps = (state) => ({
- likedSongs: state.liked.songs,
-});
+  likedSongs: state.liked.songs,
+})
 const mapDispatchToProps = (dispatch) => ({
- removeFromLiked: (songs) => dispatch(removeFromLikedAction(songs)),
-addToLiked: (songs) => dispatch(addToLikedAction(songs)),
-});
+  removeFromLiked: (songs) => dispatch(removeFromLikedAction(songs)),
+  addToLiked: (songs) => dispatch(addToLikedAction(songs)),
+})
 
-const AlbumCard = ({ song , likedSongs, addToLiked, removeFromLiked }) => {
-  const isLiked = likedSongs.includes(song);
-  console.log(isLiked, likedSongs);
+const AlbumCard = ({ song, likedSongs, addToLiked, removeFromLiked }) => {
+  const isLiked = likedSongs.includes(song)
+  console.log(isLiked, likedSongs)
   const toggleLiked = () => {
-    isLiked ? removeFromLiked(song) : addToLiked(song);
-  };
+    isLiked ? removeFromLiked(song) : addToLiked(song)
+  }
   return (
     <div className="col text-center" id={song?.id}>
       <Link to={"/album/" + song?.album.id}>
@@ -27,42 +27,41 @@ const AlbumCard = ({ song , likedSongs, addToLiked, removeFromLiked }) => {
         </div>
       </Link>
       <p>
-        <Link to={"/album/" + song?.album.id}>
-          
+        <span>
           <span>
-          {isLiked ? (
-            <AiFillHeart
-              size={22}
-              className="likeheart"
-              onClick={toggleLiked}
-            />
-          ) : (
-            <BsHeart
-              size={22}
-              className=" me-4 my-auto likeheart"
-              onClick={toggleLiked}
-            />
-          )}
-        </span>
-        <span>Album:&nbsp;</span>
-          <span>
-            "
-            {song?.album.title.length < 16
-              ? song?.album.title
-              : song?.album.title.substring(0, 16) + "..."}
-            "
+            {isLiked ? (
+              <AiFillHeart
+                size={22}
+                className="likeheart"
+                onClick={toggleLiked}
+              />
+            ) : (
+              <BsHeart
+                size={22}
+                className=" me-4 my-auto likeheart"
+                onClick={toggleLiked}
+              />
+            )}
           </span>
-          <br />
-        </Link>
-
+          <Link to={"/album/" + song?.album.id}>
+            <span>Album:&nbsp;</span>
+            <span>
+              "
+              {song?.album.title.length < 16
+                ? song?.album.title
+                : song?.album.title.substring(0, 16) + "..."}
+              "
+            </span>
+            <br />
+          </Link>
+        </span>
         <Link to={"/artist/" + song?.artist.id}>
           <span>Artist:&nbsp;</span>
           <span>{song?.artist.name}</span>
         </Link>
       </p>
     </div>
-  );
-};
+  )
+}
 
-
-export default connect(mapStateToProps, mapDispatchToProps)(AlbumCard);
+export default connect(mapStateToProps, mapDispatchToProps)(AlbumCard)
